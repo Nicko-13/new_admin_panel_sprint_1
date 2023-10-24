43ea8465fdd9ab2833ae2b35690066b50d0fbe07
Nicko-13 <schyokinn.n@gmail.com>
2023-10-24 22:08:02 +0300

    movies app added

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local apps
+    'movies.apps.MoviesConfig',
 ]
 
 MIDDLEWARE = [
