1b577566f39d846a8e8fa62d3f63037eb70665dd
Nicko-13 <schyokinn.n@gmail.com>
2023-10-25 10:57:15 +0300

    ManyToMany field added to Filmwork model, GenreFilmwork model added and registered

--- movies/admin.py
+++ movies/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from .models import Genre, Filmwork
+from .models import Genre, Filmwork, GenreFilmwork
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     pass
 
+class GenreFilmworkInline(admin.TabularInline):
+    model = GenreFilmwork
+
+
 @admin.register(Filmwork)
 class FilmworkAdmin(admin.ModelAdmin):
-    pass
-
+    inlines = (GenreFilmworkInline,)
