e4c783a89758983c8e6a78867ba3eff61841d1d3
Nicko-13 <schyokinn.n@gmail.com>
2023-11-07 15:12:14 +0300

    directories and files from previous sprint removed

--- migrate.py
+++
@@ -1,5 +0,0 @@
-from sqlite_to_postgres.data_migration import DataMigration
-
-if __name__ == '__main__':
-    migration = DataMigration()
-    migration.migrate()
--- sqlite_to_postgres/database_entries/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import BaseEntry
-from .film_work_entry import FilmWorkEntry
-from .genre_entry import GenreEntry
-from .genre_film_work_entry import GenreFilmWorkEntry
-from .person_entry import PersonEntry
-from .person_film_work_entry import PersonFilmWorkEntry
