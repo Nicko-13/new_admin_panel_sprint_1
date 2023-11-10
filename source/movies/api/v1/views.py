157e0aa4cd8c1fe5aa0b7e7d9587a33a14d37e51
Nicko-13 <schyokinn.n@gmail.com>
2023-11-10 18:22:13 +0300

    movies/api/v1/views.py updated with boilerplate code

--- source/movies/api/v1/views.py
+++ source/movies/api/v1/views.py
@@ -1,10 +1,37 @@
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import Q
 from django.http import JsonResponse
-from django.views import View
+from django.views.generic.detail import BaseDetailView
+from django.views.generic.list import BaseListView
+from movies.models import Filmwork
 
 
-class MoviesListApi(View):
+class MoviesApiMixin:
+    model = Filmwork
     http_method_names = ['get']
 
-    def get(self, request, *args, **kwargs):
-        # Получение и обработка данных
-        return JsonResponse({})
+    def get_queryset(self):
+        return  # Сформированный QuerySet
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
+
+
+class MoviesListApi(MoviesApiMixin, BaseListView):
+    def get_context_data(self, *, object_list=None, **kwargs):
+        queryset = self.get_queryset()
+        paginator, page, queryset, is_paginated = self.paginate_queryset(
+            queryset, self.paginate_by
+        )
+        # some code
+        return {
+            'count': paginator.count,
+        }
+
+
+class MoviesDetailApi(BaseDetailView):
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = {
+            'results': list(self.get_queryset()),
+        }
+        return context
