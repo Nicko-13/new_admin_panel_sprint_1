70cd92c990b64b43e55f294b08d323fb5ee15a72
Nicko-13 <schyokinn.n@gmail.com>
2023-11-13 14:50:28 +0300

    MoviesApiMixin returned appropriate Queryset, key in values method

--- source/movies/api/v1/views.py
+++ source/movies/api/v1/views.py
@@ -1,5 +1,6 @@
 from django.contrib.postgres.aggregates import ArrayAgg
 from django.db.models import Q
+from django.db.models.query import QuerySet
 from django.http import JsonResponse
 from django.views.generic.detail import BaseDetailView
 from django.views.generic.list import BaseListView
@@ -10,8 +11,11 @@ class MoviesApiMixin:
     model = Filmwork
     http_method_names = ['get']
 
-    def get_queryset(self):
-        return  # Сформированный QuerySet
+    def get_queryset(self) -> QuerySet:
+        """
+        Возвращает подготовленный Queryset в соответствии с запросом.
+        """
+        return self.model.objects.all().values()
 
     def render_to_response(self, context, **response_kwargs):
         return JsonResponse(context)
@@ -19,14 +23,10 @@ class MoviesApiMixin:
 
 class MoviesListApi(MoviesApiMixin, BaseListView):
     def get_context_data(self, *, object_list=None, **kwargs):
-        queryset = self.get_queryset()
-        paginator, page, queryset, is_paginated = self.paginate_queryset(
-            queryset, self.paginate_by
-        )
-        # some code
-        return {
-            'count': paginator.count,
+        context = {
+            'results': list(self.get_queryset()),
         }
+        return context
 
 
 class MoviesDetailApi(BaseDetailView):
